c11015560ff703908a649b61fcb8fee759dc6edc
mohammad-SSA <mohammad.alatawneh@gmail.com>
2025-10-22 08:59:06 +0400

    KAN-1

--- SQL/XX_TEST_SQL_FILE2.sql
+++ SQL/XX_TEST_SQL_FILE2.sql
@@ -1,3 +1,3 @@
 CREATE TABLE xx_st.xx_hrms_otl_int_tab_hist_arch1
 AS SELECT * FROM xx_st.xx_hrms_otl_int_tab_hist
-WHERE trunc(creation_date) <= add_months(sysdate, - 12);
+WHERE trunc(creation_date) <= add_months(sysdate, - 24);
